02e2cd9a48bb75fffd5acf1a34b644791f0f6f2b
ferb00000100 <fhlh@protonmail.com>
2020-10-16 13:42:49 -0600

    mod4-2

--- mainapp/models.py
+++ mainapp/models.py
@@ -8,7 +8,7 @@ class BlogPost(models.Model):
     author = models.ForeignKey(settings.AUTH_USER_MODEL, related_name="posts", on_delete=models.CASCADE)
     body = models.TextField()
     postdate = models.DateTimeField(auto_now_add=True, blank=True)
-    tags = models.ManyToManyField('Tag', related_name='post')
+    tags = models.ManyToManyField('Tag', related_name='posts')
 
     def __str__(self):
         return self.title
@@ -21,7 +21,7 @@ class Tag(models.Model):
     name = models.CharField(max_length=50, unique=True)
 
     def clean(self):
-        self.name.lower()
+        self.name = self.name.lower()
 
     def __str__(self):
         return self.name
